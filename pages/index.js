d0740e25ead9341772cc507d1502ef17d6ef2456
cerca1 <cerca2273@gmail.com>
2026-08-14 09:35:34 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 export default function Home() {
   const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
   const [search, setSearch] = useState("");
   const [clientId] = useState(() => {
   if (typeof window === "undefined") return "";

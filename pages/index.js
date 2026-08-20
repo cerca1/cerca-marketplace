5e20816dcccc37376d8739f82f25c9f0db4da5fd
cerca1 <cerca2273@gmail.com>
2026-08-20 17:31:15 -0300

    Adjust layout margins and padding in index.js

--- pages/index.js
+++ pages/index.js
@@ -639,7 +639,7 @@ border: "2px solid #e5e7eb",
     maxWidth: "1200px",
     margin: "auto",
 padding: "10px 20px 45px",    display: "grid",
-    marginTop: "-300px",
+    marginTop: "-110px",
     gridTemplateColumns: "270px 1fr",
     gap: "30px",
   }}
@@ -647,6 +647,7 @@ padding: "10px 20px 45px",    display: "grid",
 <div
   style={{
     background: "white",
+    marginLeft: "-60px",
     borderRadius: "18px",
     border: "1px solid #e5e7eb",
     padding: "22px 18px",

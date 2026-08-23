1ff7bb7a7ef3d1ced4529dcd780cd08e28e6a3ee
cerca1 <cerca2273@gmail.com>
2026-08-23 19:26:46 -0300

    Adjust marginTop in index.js for layout improvement

--- pages/index.js
+++ pages/index.js
@@ -732,7 +732,7 @@ transform: "translate(-20px, -40px) scale(0.8)",
     maxWidth: "1200px",
     margin: "auto",
 padding: "10px 20px 45px",    display: "grid",
-    marginTop: "-500px",
+    marginTop: "-400px",
     gridTemplateColumns: "270px 1fr",
     gap: "40px",
   }}
